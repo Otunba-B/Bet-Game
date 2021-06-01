8b6b2d007de243f9c869ed978f2f7ea625756865
Otunba-B <bryanejiofor@ymail.com>
2021-06-01 15:58:52 +0100

    Add project files.

---
+++ BetPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Bet_Game
+{
+    public class BetPlayer
+    {
+        public string Name;
+        public int Cash;
+
+        /// <summary>
+        /// Writes the name and the amount of cash the person has to the console.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks. ");
+        }
+
+        /// <summary>
+        /// Removes a certain amount of cash from your wallet and gives to another person.
+        /// </summary>
+        /// <param name="amount">Amount of cash to give. </param>
+        /// <returns>
+        /// The amount of cash removed from my wallet, or 0 if I don't have enough cash(or if amount is invalid).
+        /// </returns>
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+            }
+
+            else if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says " + " I don't have enough money to give you " + amount);
+                return 0;
+            }
+
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Receives a valid amount. 
+        /// </summary>
+        /// <param name="amount">
+        /// The amount of cash added to my purse.
+        /// </param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't an amount I'll take. ");
+            }
+
+            Cash += amount;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Bet_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            BetPlayer player = new BetPlayer() { Name = "The Player", Cash = 100 };
+
+            Console.WriteLine("Welcome to the casino. The odds are " + odds);
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("How much do you want to bet? ");
+                var howMuch = Console.ReadLine();
+
+                if(int.TryParse(howMuch, out int amount))
+                {
+                    var pot = player.GiveCash(amount) * 2;
+
+                    if(pot > 0)
+                    {
+                        if(random.NextDouble() > odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You won " + winnings);
+                            player.ReceiveCash(winnings);
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Bad luck. You lose! ");
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter an valid amount to bet");
+                }
+            }
+            Console.WriteLine("The House always wins");
+        }
+    }
+}
